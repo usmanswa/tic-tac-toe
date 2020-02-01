857c1912aab027c5d03daefdf3dad3e1cab8a760
usman.amadeus <usman.muhammad@amadeus.com>
2020-02-01 16:45:20 +0400

    Setup to expose apis

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
+const TicTacToe = require("./public/TicTacToe");
+const ticTacToe = new TicTacToe();
 
-app.use(express.static('public'));
+app.use(express.json());
+
+app.get("/restartGame", (req, res) => {
+  ticTacToe.restartGame();
+  res.send(200);
+});
+
+app.post("/playerMove", (req, res) => {
+  if (req.body.player && req.body.position) {
+    var gameState = ticTacToe.playerMove(req.body.player, req.body.position);
+    if (gameState) {
+      res.status(201).send(gameState);
+    } else {
+      res.status(500).send({ error: "Request failed!" });
+    }
+  } else {
+    res.status(400).send({ error: "Bad request!" });
+  }
+});
 
 app.listen(3000);
